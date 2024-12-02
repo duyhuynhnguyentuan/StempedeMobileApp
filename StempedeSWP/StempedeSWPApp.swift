8733b2f202a82e3d309e9a7c060fa336d8ecf825
duyhuynhnguyentuan <andyhntd2003@gmail.com>
2024-12-02 11:32:52 +0700

    Initial Commit

---
+++ StempedeSWP/StempedeSWPApp.swift
@@ -0,0 +1,17 @@
+//
+//  StempedeSWPApp.swift
+//  StempedeSWP
+//
+//  Created by Huynh Nguyen Tuan Duy on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct StempedeSWPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
